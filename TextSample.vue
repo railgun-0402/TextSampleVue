ab5792f1a4fafe6048b838d79e75e813a79a5f65
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-06-30 18:48:56 +0900

    first commit

---
+++ TextSample.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row mt-3">
+            <input type="test" v-model="text" class="form-control-inline col-6" />
+            <div class="col-5">
+                <button @click="show()" class="btn btn-primary">show</button>
+                <button @click="change()" class="btn btn-primary">change</button>
+            </div>
+            <div class="alert alert-info mt-3">{{ text }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            text: "",
+        };
+    },
+}
+</script>
